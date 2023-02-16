99f5a190bb16ab02038d58f51323037311884c66
Nikshit Chauhan <nikshit2012@gmail.com>
2023-02-16 18:18:14 +0530

    Quiz App

---
+++ app/src/main/java/com/example/myquizapp/Constants.kt
@@ -0,0 +1,57 @@
+package com.example.myquizapp
+
+object Constants {
+    fun getQuestions() : ArrayList<Question>{
+        val questionList = ArrayList<Question>()
+
+        val que1 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Argentina","Australia",
+            "Armenia","Austria",
+            2
+        )
+        questionList.add(que1)
+        val que2 = Question(
+            2, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina","Australia",
+            "Armenia","Austria",
+            1
+        )
+        questionList.add(que2)
+        val que3 = Question(
+            3, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Argentina","India",
+            "Belgium","Pakistan",
+            3
+        )
+        questionList.add(que3)
+        val que4 = Question(
+            4, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "England","Belgium",
+            "South Africa","Brazil",
+            4
+        )
+        questionList.add(que4)
+        val que5 = Question(
+            5, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "England","Denmark",
+            "Brazil","Belgium",
+            2
+        )
+        questionList.add(que5)
+        val que6 = Question(
+            6, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "India","Bangladesh",
+            "Belgium","Kuwait",
+            1
+        )
+        questionList.add(que6)
+        return questionList
+    }
+}
---
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myquizapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+        val et_name : EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+
+            if(et_name.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+    }
+}
